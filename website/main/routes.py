26cabf76e0239aca3368a3c6e537b29199c5a29f
devpedrofurquim <pedrofurquim.dev@gmail.com>
2023-07-21 01:57:54 -0300

    made many changes on the front-end

--- website/main/routes.py
+++ website/main/routes.py
@@ -1,10 +1,16 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, url_for
 from flask_login import login_required, current_user
 from .. import db
 from website.models import Note, User
 from sqlalchemy import desc
 
 main = Blueprint('main', __name__)
+
+@main.route('/foo', methods=['GET'])
+def foo():
+
+    return render_template('foo.html', user=current_user)
+
 
 @main.route('/', methods=['GET', 'POST'])
 @login_required
@@ -22,10 +28,11 @@ def home():
             db.session.add(note_text)
             db.session.commit()
             flash('Note added!', category='success')
-    
+            
+    img_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     page = request.args.get('page', 1, type=int)
-    per_page = 10  # Number of notes to display per page
+    per_page = 6  # Number of notes to display per page
     user = User.query.get(current_user.id)
     notes = Note.query.filter_by(user_id=user.id).order_by(desc(Note.date)).paginate(page=page, per_page=per_page)    
     
-    return render_template('home.html',user=current_user, notes=notes)
+    return render_template('home.html',user=current_user, notes=notes, img_file=img_file)
